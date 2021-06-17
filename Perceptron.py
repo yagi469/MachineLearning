561737980458301a7a05a9726fcb6cd497c3e8fd
Ryohei Yaginuma <55869046+yagi469@users.noreply.github.com>
2021-06-17 14:39:17 +0900

    Update Perceptron.py

--- Perceptron.py
+++ Perceptron.py
@@ -16,3 +16,35 @@ class Perceptron(object):
   w_ : 1次元配列
     適合後の重み
   errors_ : リスト
+  　各エポックでの誤分類（更新）の数
+  
+  """
+  def __init__(self, eta=0.01, n_iter=50, random_state=1):
+    self.eta = eta
+    self.n_iter = n_iter
+    self.random_state = random_state
+    
+  def fit(self, X, y):
+    """トレーニングデータに適合させる
+    
+    パラメータ
+    ---------------
+    X : ｛配列のようなデータ構造｝, shape = [n_samples, n_features]
+         トレーニングデータ
+    y : 配列のようなデータ構造,  shape = [n_samples]
+    　　　目的変数
+       
+    戻り値
+    ---------------
+    self : object
+    
+    """
+    rgen = np.random.RandomState(self.random_state)
+    self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+    self.errors_ = []
+    
+    for _ in range(self.n_iter): # トレーニング回数分トレーニングデータを反復
+      errors = 0
+      for xi, target in zip(X, y): # 各サンプルで重みを更新
+        # 重みのw_1,...w_mの更新
+        update = self.eta * (target - self.predict(xi))
